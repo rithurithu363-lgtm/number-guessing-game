52dc02bc275f4ec9bd746b2e77139904ec8a57f8
rithurithu363-lgtm <rithurithu363@gmail.com>
2025-12-05 16:12:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello! This is my first GitHub project.")
+import random
+
+print("\n🎯 Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.\n")
+
+secret_number = random.randint(1, 100)
+attempts = 0
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.\n")
+        elif guess > secret_number:
+            print("Too high! Try again.\n")
+        else:
+            print(f"\n🎉 Congratulations! You guessed it in {attempts} attempts.")
+            break
+
+    except ValueError:
+        print("Please enter a valid number.\n")
